eca9a1c5e6e8c405010b3764e4a21cd7bebfbf23
김재영 <jeay5290857@c6r2s5.codyssey.kr>
2026-06-05 16:43:46 +0900

    main

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+
+def main():
+    parser = create_parser()
+
+    try:
+        args = parser.parse_args()
+    except SystemExit as e: #수정필요!!!!
+        sys.exit(e.code)
+
+    if args.command == "add":
+        pass
+    elif args.command == "":
+        pass
+    elif args.command == "":
+        pass
+    elif args.command == "":
+        pass
+    else:
+        print("알 수 없는 명령어")
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
